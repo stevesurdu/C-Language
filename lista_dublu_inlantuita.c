259c2d06eaf1f08e2e7856793e86733cbbaf89ba
Voloc Igor <igorvoloc@gmail.com>
2015-04-21 10:46:58 +0300

    Update lista_dublu_inlantuita.c

--- lista_dublu_inlantuita.c
+++ lista_dublu_inlantuita.c
@@ -46,9 +46,49 @@ void afisareDS(listad *q){
         p = p->prev;
     }
 }
+listad *inserare(listad *q,int poz,int x){
+    listad *p, *nou;
+    int i;
+
+    nou = (listad*) malloc(sizeof(listad));
+    nou->inf = x;
+
+    if(poz==1){
+        nou->prev =NULL;
+        nou->next = q;
+        if(q) q->prev = nou;
+        q=nou;
+    }else{
+        p = q;
+        for(i=1;i<poz-1;i++)
+            p = p->next;
+        p->next->prev = nou;
+        nou->next = p->next;
+        p->next = nou;
+        nou->prev = p;
+    }
+    n++;
+    return q;
+}
+/*listad *stergere(){
+    listad *p = q;
+
+    s = p->next;
+    p->next = s->next;
+    s->next->prev = p;
+
+    free(s);
+
+    return q;
+}*/
 int main(){
     listad *q;
     q = creare();
+    afisareSD(q);
+    afisareDS(q);
+    printf("\nLungimea listei:%d",n);
+
+    q = inserare(q,3,100);
     afisareSD(q);
     afisareDS(q);
     printf("\nLungimea listei:%d",n);
