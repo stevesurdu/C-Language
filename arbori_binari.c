62a3e1f2f5b87bf678b15913856e14796c9367d9
Voloc Igor <igorvoloc@gmail.com>
2015-04-28 11:22:44 +0300

    Update arbori_binari.c

--- arbori_binari.c
+++ arbori_binari.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 typedef struct arbore{
     int inf;
     struct arbore *st,*dr;
 }arb;
+int s=0,k=0,h=0,max = INT_MIN;
 arb *creare(){
     arb *a;
     char r;
-    printf("\n Nod NULL?(D?N");
+    printf("\n Nod NULL?(D?N)");
     r = getch();
     if(r=='n'||r=='N'){
             a = (arb*)malloc(sizeof(arb));
@@ -43,13 +45,53 @@ void SDR(arb *a){
         printf("%d ",a->inf);
     }
 }
-int mian(){
+int suma(arb *a){
+    if(a){
+        s+=a->inf;
+        s=suma(a->st);
+        s=suma(a->dr);
+    }
+    return s;
+}
+int maxim(arb *a){
+    if(a){
+        if(a->inf>max)max=a->inf;
+        max=maxim(a->st);
+        max=maxim(a->dr);
+    }
+    return max;
+}
+int frunze(arb *a){
+    if(a){
+        if(a->st==NULL && a->dr==NULL) k++;
+        k=frunze(a->st);
+        k=frunze(a->dr);
+    }
+    return k;
+}
+int inaltime(arb *a){
+    int hs,hd;
+    if(a){
+        hs = inaltime(a->st);
+        hs = inaltime(a->st);
+        if(hs>hd) return hs+=1;
+        else return hd+=1;
+    }else{
+        return 0;
+    }
+}
+int main(){
     arb *a;
 
     a = creare();
     printf("\nRSD:");RSD(a);
     printf("\nSRD:");SRD(a);
     printf("\nSDR:");SDR(a);
+
+    printf("\nSuma:%d",suma(a));
+    printf("\nMaxim:%d",maxim(a));
+    printf("\nNr frunze:%d",frunze(a));
+    printf("\nInaltimea:%d",inaltime(a));
 
     return 0;
 }
